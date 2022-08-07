3567414926cb2cd6db5687308a892cada1140ce2
arinn <arinn1250@gmail.com>
2022-08-06 17:34:53 -0700

    returning the environment for use by callers

--- internal/java/java.go
+++ internal/java/java.go
@@ -10,21 +10,22 @@ import (
 var jvm *jnigi.JVM
 
 // CreateJVM will create a JVM for the consumer to execute against
-func CreateJvm() error {
+func CreateJvm() (*jnigi.Env, error) {
 	if err := jnigi.LoadJVMLib(jnigi.AttemptToFindJVMLibPath()); err != nil {
-		return errors.New("Failed to create a JVM::" + err.Error())
+		return nil, errors.New("Failed to create a JVM::" + err.Error())
 	}
 
 	runtime.LockOSThread()
 	var err error
+	var env *jnigi.Env
 
-	jvm, _, err = jnigi.CreateJVM(jnigi.NewJVMInitArgs(false, true, jnigi.DEFAULT_VERSION, []string{"-Xcheck:jni"}))
+	jvm, env, err = jnigi.CreateJVM(jnigi.NewJVMInitArgs(false, true, jnigi.DEFAULT_VERSION, []string{"-Xcheck:jni"}))
 
 	if err != nil {
-		return errors.New("Failed to create the JVM::" + err.Error())
+		return nil, errors.New("Failed to create the JVM::" + err.Error())
 	}
 
-	return nil
+	return env, nil
 }
 
 func ShutdownJvm() {
