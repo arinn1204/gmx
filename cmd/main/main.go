c0be2b17c5da7e488c3c55550c8de8fa3ecf72b4
arinn <arinn1250@gmail.com>
2022-08-07 17:45:13 -0700

    easy usage example

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,9 +1,46 @@
 package main
 
 import (
-	"gmx/pkg/gmx"
+	"fmt"
+	"gmx/internal/java"
 )
 
 func main() {
-	gmx.DoWork()
+
+	mbean := &java.MBean{}
+
+	err := mbean.InitializeMBeanConnection("service:jmx:rmi:///jndi/rmi://127.0.0.1:9001/jmxrmi")
+	defer mbean.Close()
+	operation := java.MBeanOperation{
+		Domain:    "org.example",
+		Name:      "game",
+		Operation: "putString",
+		Args: []java.MBeanOperationArgs{
+			{
+				Value: "messi",
+				Type:  "java.lang.String",
+			},
+			{
+				Value: "fan369",
+				Type:  "java.lang.String",
+			},
+		},
+	}
+	mbean.Execute(operation)
+
+	operation = java.MBeanOperation{
+		Domain:    "org.example",
+		Name:      "game",
+		Operation: "getString",
+		Args: []java.MBeanOperationArgs{
+			{
+				Value: "messi",
+				Type:  "java.lang.String",
+			},
+		},
+	}
+	res, err := mbean.Execute(operation)
+
+	fmt.Println(res)
+	fmt.Println(err)
 }
