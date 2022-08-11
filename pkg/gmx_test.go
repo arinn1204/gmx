437c7ebcf3b361d1798fc71188e56e2eae638cf6
arinn <arinn1250@gmail.com>
2022-08-11 08:09:46 -0700

    testing against the mass operation

--- pkg/gmx_test.go
+++ pkg/gmx_test.go
@@ -57,3 +57,41 @@ func TestExecuteAgainstID(t *testing.T) {
 
 	}
 }
+
+func TestExecuteAgainstAll(t *testing.T) {
+	expectedOperation := make(map[uuid.UUID]mbean.Operation)
+	operation := mbean.Operation{
+		Domain:    "com.google",
+		Name:      "spyware",
+		Operation: "getLocation",
+		Args:      make([]mbean.OperationArgs, 0),
+	}
+
+	locationId := uuid.New()
+	gameId := uuid.New()
+
+	expectedOperation[locationId] = operation
+
+	gameExecutor := mbean.MockBeanExecutor{}
+	gameExecutor.On("Execute", operation).Return("NV", nil)
+
+	locationExecutor := mbean.MockBeanExecutor{}
+	locationExecutor.On("Execute", operation).Return("CA", nil)
+
+	mbeans := make(map[uuid.UUID]mbean.BeanExecutor)
+	mbeans[locationId] = &locationExecutor
+	mbeans[gameId] = &gameExecutor
+
+	client := Client{
+		mbeans: mbeans,
+	}
+
+	res, err := client.ExecuteAgainstAll("com.google", "spyware", "getLocation")
+
+	assert.Nil(t, err[gameId])
+	assert.Nil(t, err[locationId])
+
+	assert.Equal(t, "NV", res[gameId])
+	assert.Equal(t, "CA", res[locationId])
+
+}
