9fb3aeeadebd2b50b9d16969c0991ef8153e4582
arinn <arinn1250@gmail.com>
2022-08-14 21:52:52 -0700

    initial commit

--- internal/handlers/list.go
+++ internal/handlers/list.go
@@ -1,1 +1,22 @@
 package handlers
+
+import "tekao.net/jnigi"
+
+const (
+	ListJniRepresentation = "java/util/List"
+	ListClassPath         = "java.util.List"
+)
+
+// ListHandler is the type that will be able to convert lists to and from go arrays
+type ListHandler struct{}
+
+// ToJniRepresentation is the implementation that will convert from a go type
+// to a JNI representation of that type
+func (handler *ListHandler) ToJniRepresentation(env *jnigi.Env, value any) (*jnigi.ObjectRef, error) {
+	return nil, nil
+}
+
+// ToGoRepresentation will convert from a JNI type to a go type
+func (handler *ListHandler) ToGoRepresentation(env *jnigi.Env, object *jnigi.ObjectRef, dest any) error {
+	return nil
+}
