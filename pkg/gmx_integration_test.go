6fe9647bc28ac436945fd73a622c52755221a661
arinn <arinn1250@gmail.com>
2022-08-27 11:25:51 -0700

    can run connections and handler registration in parallel

--- pkg/gmx_integration_test.go
+++ pkg/gmx_integration_test.go
@@ -4,6 +4,7 @@ import (
 	"sync"
 	"testing"
 
+	"github.com/arinn1204/gmx/internal/handlers"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 )
@@ -12,7 +13,6 @@ func TestCanMakeMultipleAccountsInParralel(t *testing.T) {
 	if testing.Short() {
 		return
 	}
-
 	gmxClient := CreateClient()
 	wg := sync.WaitGroup{}
 
@@ -25,6 +25,38 @@ func TestCanMakeMultipleAccountsInParralel(t *testing.T) {
 		go func(wg *sync.WaitGroup) {
 			defer wg.Done()
 			id, err := gmxClient.Connect("127.0.0.1", 9001)
+
+			assert.Nil(t, err)
+			ids = append(ids, *id)
+		}(&wg)
+
+	}
+
+	wg.Wait()
+
+	assert.Equal(t, totalConnections, len(ids))
+
+	c := gmxClient.(*client)
+	assert.Equal(t, uint(totalConnections), c.numberOfConnections)
+}
+
+func TestCanMakeMultipleAccountsInParralelAndRegisterHandlers(t *testing.T) {
+	if testing.Short() {
+		return
+	}
+	gmxClient := CreateClient()
+	wg := sync.WaitGroup{}
+
+	ids := make([]uuid.UUID, 0)
+
+	totalConnections := 25
+
+	for i := 0; i < totalConnections; i++ {
+		wg.Add(1)
+		go func(wg *sync.WaitGroup) {
+			defer wg.Done()
+			id, err := gmxClient.Connect("127.0.0.1", 9001)
+			gmxClient.RegisterClassHandler(handlers.BoolClasspath, &handlers.BoolHandler{})
 
 			assert.Nil(t, err)
 			ids = append(ids, *id)
